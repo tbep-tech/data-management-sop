14153f2cca558ab97efd505aaff59037d63c82b5
fawda123 <mbafs2012@gmail.com>
2021-08-20 08:14:48 -0400

    pdf build with new cover sheet

--- _build.sh
+++ _build.sh
@@ -3,6 +3,9 @@
 set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "pgs <- qpdf::pdf_length('_book/data-management-sop.pdf')"
+Rscript -e "qpdf::pdf_subset('_book/data-management-sop.pdf', pages = c(2:pgs), output = '_book/data-management-sop.pdf')"
+Rscript -e "qpdf::pdf_combine(c('www/SOP Cover.pdf', '_book/data-management-sop.pdf'), output = '_book/data-management-sop.pdf')"
 
